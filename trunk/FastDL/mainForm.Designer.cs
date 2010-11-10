9e73a43ced65811b665938f2726678af4cc87192
anatole.korczak <anatole.korczak@81981892-611f-0018-ee56-448df449a0ed>
2010-11-10 20:12:16 +0000

    Ajout de la recherche ( pas encore fonctionnel )

--- trunk/FastDL/mainForm.Designer.cs
+++ trunk/FastDL/mainForm.Designer.cs
@@ -49,17 +49,28 @@
             this.lblDlState = new System.Windows.Forms.Label();
             this.tcMain = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tbSearch = new System.Windows.Forms.RichTextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvSearch = new System.Windows.Forms.DataGridView();
+            this.Filename = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.url = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.nudBlockSize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudConnPerInterfaces)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvThread)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbProgressBlock)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvInterfaces)).BeginInit();
             this.tcMain.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearch)).BeginInit();
             this.SuspendLayout();
             // 
             // nudBlockSize
             // 
-            this.nudBlockSize.Location = new System.Drawing.Point(621, 286);
+            this.nudBlockSize.Location = new System.Drawing.Point(620, 375);
             this.nudBlockSize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.nudBlockSize.Name = "nudBlockSize";
             this.nudBlockSize.Size = new System.Drawing.Size(140, 23);
@@ -72,7 +83,7 @@
             // 
             // nudConnPerInterfaces
             // 
-            this.nudConnPerInterfaces.Location = new System.Drawing.Point(621, 326);
+            this.nudConnPerInterfaces.Location = new System.Drawing.Point(620, 415);
             this.nudConnPerInterfaces.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.nudConnPerInterfaces.Name = "nudConnPerInterfaces";
             this.nudConnPerInterfaces.Size = new System.Drawing.Size(140, 23);
@@ -93,7 +104,7 @@
             this.bytesdownloaded,
             this.percentblock,
             this.part});
-            this.dgvThread.Location = new System.Drawing.Point(42, 161);
+            this.dgvThread.Location = new System.Drawing.Point(9, 95);
             this.dgvThread.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.dgvThread.Name = "dgvThread";
             this.dgvThread.ReadOnly = true;
@@ -129,7 +140,7 @@
             // lblBlockSize
             // 
             this.lblBlockSize.AutoSize = true;
-            this.lblBlockSize.Location = new System.Drawing.Point(519, 288);
+            this.lblBlockSize.Location = new System.Drawing.Point(518, 377);
             this.lblBlockSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblBlockSize.Name = "lblBlockSize";
             this.lblBlockSize.Size = new System.Drawing.Size(90, 15);
@@ -139,7 +150,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(493, 329);
+            this.label2.Location = new System.Drawing.Point(492, 418);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(114, 15);
@@ -171,7 +182,7 @@
             // 
             this.lblSpeed.AutoSize = true;
             this.lblSpeed.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSpeed.Location = new System.Drawing.Point(12, 73);
+            this.lblSpeed.Location = new System.Drawing.Point(8, 7);
             this.lblSpeed.Name = "lblSpeed";
             this.lblSpeed.Size = new System.Drawing.Size(86, 23);
             this.lblSpeed.TabIndex = 7;
@@ -180,7 +191,7 @@
             // pbProgressBlock
             // 
             this.pbProgressBlock.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.pbProgressBlock.Location = new System.Drawing.Point(13, 98);
+            this.pbProgressBlock.Location = new System.Drawing.Point(9, 32);
             this.pbProgressBlock.Name = "pbProgressBlock";
             this.pbProgressBlock.Size = new System.Drawing.Size(700, 30);
             this.pbProgressBlock.TabIndex = 8;
@@ -188,7 +199,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(687, 416);
+            this.button2.Location = new System.Drawing.Point(687, 427);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 41);
             this.button2.TabIndex = 9;
@@ -235,7 +246,7 @@
             // lblDlState
             // 
             this.lblDlState.AutoSize = true;
-            this.lblDlState.Location = new System.Drawing.Point(16, 140);
+            this.lblDlState.Location = new System.Drawing.Point(12, 74);
             this.lblDlState.Name = "lblDlState";
             this.lblDlState.Size = new System.Drawing.Size(0, 15);
             this.lblDlState.TabIndex = 11;
@@ -243,10 +254,12 @@
             // tcMain
             // 
             this.tcMain.Controls.Add(this.tabPage1);
-            this.tcMain.Location = new System.Drawing.Point(522, 190);
+            this.tcMain.Controls.Add(this.tabPage2);
+            this.tcMain.Controls.Add(this.tabPage3);
+            this.tcMain.Location = new System.Drawing.Point(12, 68);
             this.tcMain.Name = "tcMain";
             this.tcMain.SelectedIndex = 0;
-            this.tcMain.Size = new System.Drawing.Size(99, 34);
+            this.tcMain.Size = new System.Drawing.Size(750, 301);
             this.tcMain.TabIndex = 12;
             // 
             // tabPage1
@@ -254,23 +267,96 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 24);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(91, 6);
+            this.tabPage1.Size = new System.Drawing.Size(742, 273);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Téléchargements";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dgvThread);
+            this.tabPage2.Controls.Add(this.lblDlState);
+            this.tabPage2.Controls.Add(this.pbProgressBlock);
+            this.tabPage2.Controls.Add(this.lblSpeed);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(742, 273);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "temporaire";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dgvSearch);
+            this.tabPage3.Controls.Add(this.btnSearch);
+            this.tabPage3.Controls.Add(this.tbSearch);
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(742, 273);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Recherche";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(74, 24);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(352, 31);
+            this.tbSearch.TabIndex = 0;
+            this.tbSearch.Text = "";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(459, 24);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(145, 31);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search on Megaupload";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvSearch
+            // 
+            this.dgvSearch.BackgroundColor = System.Drawing.Color.PowderBlue;
+            this.dgvSearch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSearch.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Filename,
+            this.Description,
+            this.url});
+            this.dgvSearch.Location = new System.Drawing.Point(6, 61);
+            this.dgvSearch.Name = "dgvSearch";
+            this.dgvSearch.RowHeadersVisible = false;
+            this.dgvSearch.Size = new System.Drawing.Size(730, 197);
+            this.dgvSearch.TabIndex = 2;
+            // 
+            // Filename
+            // 
+            this.Filename.HeaderText = "Filename";
+            this.Filename.Name = "Filename";
+            this.Filename.Width = 170;
+            // 
+            // Description
+            // 
+            this.Description.HeaderText = "Description";
+            this.Description.Name = "Description";
+            this.Description.Width = 250;
+            // 
+            // url
+            // 
+            this.url.HeaderText = "URL";
+            this.url.Name = "url";
+            this.url.Width = 300;
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(774, 469);
-            this.Controls.Add(this.dgvThread);
             this.Controls.Add(this.tcMain);
-            this.Controls.Add(this.lblDlState);
             this.Controls.Add(this.dgvInterfaces);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.pbProgressBlock);
-            this.Controls.Add(this.lblSpeed);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.rtbURL);
             this.Controls.Add(this.label2);
@@ -288,6 +374,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbProgressBlock)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvInterfaces)).EndInit();
             this.tcMain.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearch)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -316,6 +406,14 @@
         private System.Windows.Forms.Label lblDlState;
         private System.Windows.Forms.TabControl tcMain;
         private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dgvSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.RichTextBox tbSearch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Filename;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn url;
     }
 }
 
