d4c3971b16cff53496dffb93e74577e5f04bf8d7
anat <anatole.korczak@gmail.com>
2015-03-21 04:01:41 +0100

    Update Search.cs

--- FastDL/MISC/Search.cs
+++ FastDL/MISC/Search.cs
@@ -51,64 +51,6 @@ namespace FastDL.MISC
             return list;
         }
 
-        private List<SearchResult> Megaupload()
-        {
-            return GoogleResult("site:megaupload.com " + _words);
-        }
-
-
-        private List<SearchResult> GoogleResult(string query)
-        {
-            List<SearchResult> list = new List<SearchResult>();
-            string url = "http://www.google.fr/search?q=" + query + "&num=100";
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream s = response.GetResponseStream();
-            byte[] buff = new byte[50001];
-            string str = "";
-            int read = 1;
-            while (read != 0)
-            {
-                read = s.Read(buff, 0, 5000);
-                str += ASCIIEncoding.ASCII.GetString(buff, 0, read);
-            }
-
-            string token = "http://www.megaupload.com/?d=";
-            int i = 0;
-
-            // Get All urls
-            while (i != -1 && i + token.Length + 8 < str.Length)
-            {
-                if (str.IndexOf(token, i) != -1)
-                {
-                    list.Add(new SearchResult(str.Substring(str.IndexOf(token, i), token.Length + 8)));
-                    i = str.IndexOf(token, i) + token.Length + 1;
-                }
-                else
-                {
-                    i = -1;
-                }
-                    // MessageBox.Show(str.Substring(str.IndexOf(token, i), token.Length + 8));
-            }
-
-            // Get Description and Filename
-            foreach (SearchResult sr in list)
-            {
-                string page = DL.URLManager.megaupload(sr.url);
-
-                sr.name =  DL.URLManager.getMegauploadURL(page);
-                sr.name = sr.name.Substring(sr.name.LastIndexOf("/"));
-                sr.desc = DL.URLManager.getMegauploadDesc(page);
-                MessageBox.Show("Name : " + sr.name + "\nDesc : " + sr.desc + "\nURL : " + sr.url);
-            }
-
-            return list;
-        }
-
-
         private List<SearchResult> downloadsnl()
         {
             string url = "http://www.downloads.nl/results/mp3/1/" + _words.Replace(" ", "+");
